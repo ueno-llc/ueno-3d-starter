bb5ee33aae7cc7a24d8dfc26446b659a8bc65356
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-09-02 16:45:55 +0000

    fix seo stuff

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,6 @@ import { SEO } from '../components/seo/SEO';
 export default function NotFoundPage() {
   return (
     <Layout>
-      <SEO title="404: Not found" />
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Canvas } from 'react-three-fiber';
+import styled from 'styled-components';
 import { Controls } from '../components/controls/Controls';
 import { HotDog } from '../components/hot-dog/HotDog';
 import { Layout } from '../components/layout/Layout';
+
+const MainCanvas = styled(Canvas)`
+  width: 100%;
+  height: 400px;
+  background-color: #f8f8f8;
+`;
 
 export default function IndexPage() {
   return (
     <Layout>
       <h1>Welcome</h1>
-      <Canvas
-        style={{ width: '100%', height: 400, backgroundColor: '#f8f8f8' }}
-      >
+      <MainCanvas>
         <ambientLight intensity={0.5} />
         <spotLight intensity={0.8} position={[300, 300, 400]} />
         <HotDog />
         <Controls />
-      </Canvas>
+      </MainCanvas>
     </Layout>
   );
 }
